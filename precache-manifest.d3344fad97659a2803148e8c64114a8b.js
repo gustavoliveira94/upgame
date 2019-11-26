797ce806893a7101ce4bdd331acc5913a029e94c
Gustavo Oliveira <gusttavo@live.com>
2019-11-25 23:14:46 -0300

    Updates

---
+++ precache-manifest.d3344fad97659a2803148e8c64114a8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a772e71c528a19d5271324a303ab55f",
+    "url": "/upgame/index.html"
+  },
+  {
+    "revision": "1c01b8cba3affb74b9ab",
+    "url": "/upgame/static/js/2.53b22c85.chunk.js"
+  },
+  {
+    "revision": "bd495973d7c7cb5739ae",
+    "url": "/upgame/static/js/main.4a6cc1c8.chunk.js"
+  },
+  {
+    "revision": "7f6be3c87e8a48ca7c95",
+    "url": "/upgame/static/js/runtime-main.9d70fad2.js"
+  },
+  {
+    "revision": "56be0a4e7be4b661072436aa22f66f64",
+    "url": "/upgame/static/media/CARRO.56be0a4e.png"
+  },
+  {
+    "revision": "d0d2cd1752d215c4df68e76e976ec629",
+    "url": "/upgame/static/media/CENARIO_anima.d0d2cd17.gif"
+  },
+  {
+    "revision": "513c0f196cbef93148734f610b35118f",
+    "url": "/upgame/static/media/turbo.513c0f19.gif"
+  }
+]);
